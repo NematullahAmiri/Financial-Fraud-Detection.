e051737978e43724e67f4b2b60f326b6c9193898
Nematullah Amiri <nemat_2929@yahoo.com>
2026-06-21 14:47:34 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Financial Fraud Detection Platform",
+    page_icon="🛡️",
+    layout="wide"
+)
+
+st.title("🛡️ Financial Fraud Detection Platform")
+
+st.write("Fraud Detection System")
